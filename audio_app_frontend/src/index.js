34f4e152b8d289b7bceb72509ddd827546d9d7e1
ws903 <ws903@nyu.edu>
2018-12-04 10:51:36 -0500

    audio app

--- audio_app_frontend/src/index.js
+++ audio_app_frontend/src/index.js
@@ -1,17 +1,16 @@
 const link = 'https://www.youtube.com/watch?v=R_ViOLgvsuY'
-const id = link.split('=')[1]
-const key = 'AIzaSyBEgPkKkUzYcRYR3mL5_LPbdGlrB2Nv2L8'
 
-function fetch_data(id) {
-	// var url = 'https://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json-in-script&callback=?';
-	var url = "https://www.googleapis.com/youtube/v3/videos?id=" + id + "&key=" + key + "&part=snippet,statistics,contentDetails"
+function fetch_data(link) {
+	const id = link.split('=')[1]
+	const key = 'AIzaSyBEgPkKkUzYcRYR3mL5_LPbdGlrB2Nv2L8'
+	const url = "https://www.googleapis.com/youtube/v3/videos?id=" + id + "&key=" + key + "&part=snippet,statistics,contentDetails"
 
-	debugger
-
-	// $.getJSON(url, function (data) {
-	// 	$('input#DataProvider_Title').val(data.entry.title.$t);
-	// 	$('textarea#DataProvider_Description').val(data.entry.media$group.media$description.$t);
-	// });
+	fetch(url)
+	.then(resp => resp.json())
+	.then(function(json) {
+		const response = json.items[0]['snippet']
+		const title = response.title
+	})
 }
 
-fetch_data(id)
+fetch_data(link)
