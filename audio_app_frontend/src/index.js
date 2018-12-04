dd6524ce2cf27c1e40df13348f81d31eb7ddb8a1
ws903 <ws903@nyu.edu>
2018-12-04 11:12:25 -0500

    added player

--- audio_app_frontend/src/index.js
+++ audio_app_frontend/src/index.js
@@ -1,6 +1,6 @@
-const link = 'https://www.youtube.com/watch?v=R_ViOLgvsuY'
+const link = 'https://www.youtube.com/watch?v=KfXvjxbRhZk'
 
-function fetch_data(link) {
+function fetchSong(link) {
 	const id = link.split('=')[1]
 	const key = 'AIzaSyBEgPkKkUzYcRYR3mL5_LPbdGlrB2Nv2L8'
 	const url = "https://www.googleapis.com/youtube/v3/videos?id=" + id + "&key=" + key + "&part=snippet,statistics,contentDetails"
@@ -8,9 +8,27 @@ function fetch_data(link) {
 	fetch(url)
 	.then(resp => resp.json())
 	.then(function(json) {
-		const response = json.items[0]['snippet']
-		const title = response.title
+		addSongToHtml(json)
 	})
 }
 
-fetch_data(link)
+function addSongToHtml(song) {
+	const response = song.items[0]['snippet']
+	const title = response.title
+	const artist = title.split(' - ')[0]
+
+	const id = link.split('=')[1]
+	const playLink = `https://www.youtube.com/embed/${id}`
+
+	const ul = document.querySelector('#song-list')
+	const li = document.createElement('li')
+	li.innerText = title
+	ul.appendChild(li)
+
+	li.addEventListener('click', function() {
+		const player = document.querySelector('#audio-player')
+		player.src = `${playLink}`
+	})
+}
+
+fetchSong(link)
