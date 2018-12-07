2d4aa2996e7afd7ccbdae75929f180fcb0134240
hmalik88 <hbmalik88@gmail.com>
2018-12-07 02:44:22 -0500

    some more cleaning up

--- audio_app_frontend/index.js
+++ audio_app_frontend/index.js
@@ -7,7 +7,7 @@ let currentSong = 0;
 let currentPoster = 0;
 const songsUrl = 'http://localhost:3000/api/v1/songs'
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#song-form')
   const playlist = document.querySelector('#playlist')
   playlist.addEventListener('click', changeSongFromPL)
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 })
 
-function posterCheck(id) {
+const posterCheck = id => {
   let parsedId = parseInt(id);
   if (parsedId === 1) {
       return 0;
@@ -35,7 +35,7 @@ function posterCheck(id) {
     }
 }
 
-function changeSongFromPL(e) {
+const changeSongFromPL = e => {
   if (e.target.className === 'track-span') {
     let id = e.target.parentElement.dataset.id;
     currentPoster = posterCheck(id);
@@ -45,11 +45,10 @@ function changeSongFromPL(e) {
     $("#image img").attr("src",poster[currentPoster]);
     $("#bg img").attr("src",poster[currentPoster]);
     $("#play img").attr("src","Pause.png");
-
   }
 }
 
-function submitSong(song) {
+const submitSong = song => {
   const data = {
     song: {
       song_file: song.song_file.value.replace("C:\\fakepath\\", "")
@@ -63,15 +62,13 @@ function submitSong(song) {
     body: JSON.stringify(data)
   })
   .then(resp => resp.json())
-  .then(function(song) {
-    addSongToHtml(song)
-  })
+  .then(addSongToHtml)
 }
 
-function fetchSongs() {
+const fetchSongs = () => {
   fetch(songsUrl)
   .then(resp => resp.json())
-  .then(function(json) {
+  .then(json => {
     for(const song of json) {
       addSongToHtml(song)
       songs.push(song.song_file);
@@ -82,13 +79,8 @@ function fetchSongs() {
   })
 }
 
-function addSongToHtml(song) {
+const addSongToHtml = song => {
   const ol = document.querySelector('#playlist-list')
-  // const li = document.createElement('li')
-  // li.setAttribute('data-song-id', song.id)
-  // li.innerText = song.song_file
-
-  // ol.appendChild(li)
   ol.innerHTML += `
   <div class="track-div">
   <li data-id="${song.id}"><span class="track-span">${song.song_file}</span></li>
@@ -97,69 +89,67 @@ function addSongToHtml(song) {
 }
 
 const playSong = () => {
-      song.src = `/Users/hassanmalik/downloads/${songs[currentSong]}`;
-      displaySongInfo();
-      song.play();
-  }
+  song.src = `/Users/hassanmalik/downloads/${songs[currentSong]}`;
+  displaySongInfo();
+  song.play();
+}
 
 const playOrPauseSong = () => {
-      if (song.paused) {
-          song.play();
-          $("#play img").attr("src","Pause.png");
-      }
-      else {
-          song.pause();
-          $("#play img").attr("src","Play.png");
-      }
+  if (song.paused) {
+      song.play();
+      $("#play img").attr("src","Pause.png");
   }
+  else {
+      song.pause();
+      $("#play img").attr("src","Play.png");
+  }
+}
 
-  song.addEventListener('timeupdate',function(){
-      let fillBar = document.getElementById("fill");
-      let position = song.currentTime / song.duration;
-      fillBar.style.width = position * 100 +'%';
-
-  });
+song.addEventListener('timeupdate', () => {
+  let fillBar = document.getElementById("fill");
+  let position = song.currentTime / song.duration;
+  fillBar.style.width = position * 100 +'%';
+});
 
 
 const next = () => {
-      currentSong++;
-      currentPoster++;
-      if (currentSong > songs.length - 1) {
-          currentSong = 0;
-          $("#playlist").animate({ scrollTop: '0' }, 500)
-      }
-      if (currentPoster > 2) {
-        currentPoster = 0;
-      }
-      selectedSong(currentSong);
-      playSong();
-      $("#play img").attr("src","Pause.png");
-      $("#image img").attr("src",poster[currentPoster]);
-      $("#bg img").attr("src",poster[currentPoster]);
+  currentSong++;
+  currentPoster++;
+  if (currentSong > songs.length - 1) {
+    currentSong = 0;
+    $("#playlist").animate({ scrollTop: '0' }, 500)
   }
+  if (currentPoster > 2) {
+    currentPoster = 0;
+  }
+  selectedSong(currentSong);
+  playSong();
+  $("#play img").attr("src","Pause.png");
+  $("#image img").attr("src",poster[currentPoster]);
+  $("#bg img").attr("src",poster[currentPoster]);
+}
 
 const pre = () => {
-      currentSong--;
-      currentPoster--;
-      if (currentSong < 0) {
-          currentSong = songs.length - 1;
-          $("#playlist").animate({ scrollTop: '1000' }, 500)
-      }
-      if (currentPoster < 0) {
-        currentPoster = 2
-      }
-      selectedSong(currentSong);
-      playSong();
-      $("#play img").attr("src","Pause.png");
-      $("#image img").attr("src",poster[currentPoster]);
-      $("#bg img").attr("src",poster[currentPoster]);
+  currentSong--;
+  currentPoster--;
+  if (currentSong < 0) {
+    currentSong = songs.length - 1;
+    $("#playlist").animate({ scrollTop: '1000' }, 500)
   }
+  if (currentPoster < 0) {
+    currentPoster = 2
+  }
+  selectedSong(currentSong);
+  playSong();
+  $("#play img").attr("src","Pause.png");
+  $("#image img").attr("src",poster[currentPoster]);
+  $("#bg img").attr("src",poster[currentPoster]);
+}
 
 const displaySongInfo = () => {
   let songTitle = document.querySelector('#songTitle');
   songTitle.innerText = songs[currentSong]
 }
-
 
 const selectedSong = (currentSong) => {
   let trackId = currentSong + 1;
