879255ea8bcead74c1e0b1b5aa68af216af36ec9
hmalik88 <hbmalik88@gmail.com>
2018-12-07 02:03:37 -0500

    front-end pretty much done

--- audio_app_backend/app/models/song.rb
+++ audio_app_backend/app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ApplicationRecord
 	# has_one_attached :song_file
 	validates :song_file, uniqueness: true
+
+# def song_json
+#   {
+#     name: self.name,
+#     song: self.song_file
+#   }
+# end
+
 end
